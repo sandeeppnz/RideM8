8e708e711f00cee042f8f8185f7cda599132dcf8
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-08-29 23:43:29 +1200

    Add files

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const rides = () => {
+  return (
+    <SafeAreaView>
+      <Text>home</Text>
+    </SafeAreaView>
+  )
+}
+
+export default rides;
