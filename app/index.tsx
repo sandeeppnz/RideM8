fd9741ac3a229eb691334d36e899a36581d310b0
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-08-30 01:43:11 +1200

    Add signup

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from "expo-router";
 import "./global.css";
 
 const  Index = () => {
-  return <Redirect href="/(auth)/welcome" />;
+  return <Redirect href="/(auth)/signup" />;
 }
 
 export default Index;
