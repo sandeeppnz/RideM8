e8e1101f00fb72ff49f5b6036ebdff33079af477
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-08-30 00:22:29 +1200

    Add changes to welcome

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { router } from "expo-router";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const welcome = () => {
   return (
-    <SafeAreaView>
-      <Text>welcome</Text>
+    <SafeAreaView className="flex-1 h-full items-center justify-between bg-white">
+      <TouchableOpacity
+            onPress={() => {
+                router.push('/(auth)/signup');
+            }}
+            className="w-full items-end mt-10 pr-5">
+        <Text className="text-black text-md font-JakartaBold">
+          Skip
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity className="bg-black p-4 rounded-lg mb-10 w-11/12">
+        <Text className="text-white text-center text-lg font-semibold">
+          Get Started
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default welcome;
